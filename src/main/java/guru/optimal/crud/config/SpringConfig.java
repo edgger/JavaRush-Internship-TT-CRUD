4b8e0692590a6e25a7e385dd703e769f58f31f38
edgger <germedgar@gmail.com>
2018-02-13 18:06:07 +0700

    spring mvc + hibernate win %)

--- src/main/java/guru/optimal/crud/config/SpringConfig.java
+++ src/main/java/guru/optimal/crud/config/SpringConfig.java
@@ -2,9 +2,9 @@ package guru.optimal.crud.config;
 
 import guru.optimal.crud.entity.Book;
 import guru.optimal.crud.service.TestBean;
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.AvailableSettings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.*;
 import org.springframework.core.env.Environment;
@@ -12,10 +12,9 @@ import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import javax.sql.DataSource;
 import java.io.IOException;
 import java.util.Properties;
-
-import static org.hibernate.cfg.AvailableSettings.*;
 
 
 @Configuration
@@ -31,7 +30,7 @@ public class SpringConfig {
 
     @Bean
     public TestBean getTestBean() {
-        return new TestBean("Lolka");
+        return new TestBean("Simple test");
     }
 
     @Bean
@@ -41,25 +40,11 @@ public class SpringConfig {
         return new Book();
     }
 
-/*    @Bean
-    public LocalSessionFactoryBean getSessionFactory(MetadataSources metadataSources) {
-        LocalSessionFactoryBean localSessionFactoryBean = new LocalSessionFactoryBean();
-        localSessionFactoryBean.setMetadataSources(metadataSources);
-
-        return localSessionFactoryBean;
-    }*/
-
-    @Bean
-    public MetadataSources getMetadataSources() {
-        return new MetadataSources(new StandardServiceRegistryBuilder()
-                .configure() // configures settings from hibernate.cfg.xml
-                .build());
-    }
-
     @Bean
     public SessionFactory sessionFactory() {
         LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
-        sessionFactoryBean.setMetadataSources(getMetadataSources());
+        sessionFactoryBean.setDataSource(getDataSource());
+        sessionFactoryBean.setPackagesToScan("guru.optimal.crud.entity");
         sessionFactoryBean.setHibernateProperties(hibernateProperties());
         try {
             sessionFactoryBean.afterPropertiesSet();
@@ -69,50 +54,27 @@ public class SpringConfig {
         return sessionFactoryBean.getObject();
     }
 
+    @Bean
+    public DataSource getDataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName(env.getProperty("database.driver"));
+        dataSource.setUrl(env.getProperty("database.url"));
+        dataSource.setUsername(env.getProperty("database.user"));
+        dataSource.setPassword(env.getProperty("database.password"));
+        return dataSource;
+    }
+
     private Properties hibernateProperties() {
         Properties properties = new Properties();
-        properties.put(DRIVER, env.getProperty("mysql.driver"));
-        properties.put(URL, env.getProperty("mysql.url"));
-        properties.put(USER, env.getProperty("mysql.user"));
-        properties.put(PASS, env.getProperty("mysql.password"));
-        properties.put(SHOW_SQL, env.getProperty("hibernate.show_sql"));
-        properties.put(HBM2DDL_AUTO, env.getProperty("hibernate.hbm2ddl.auto"));
-        properties.put(DIALECT, env.getProperty("hibernate.dialect"));
+        properties.put(AvailableSettings.SHOW_SQL, env.getProperty("hibernate.show_sql"));
+        properties.put(AvailableSettings.HBM2DDL_AUTO, env.getProperty("hibernate.hbm2ddl.auto"));
+        properties.put(AvailableSettings.DIALECT, env.getProperty("hibernate.dialect"));
         return properties;
     }
 
-    /*@Bean
-    public HibernateTransactionManager getTransactionManager(SessionFactory sessionFactory) {
-        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
-        transactionManager.setSessionFactory(sessionFactory);
-        return transactionManager;
-    }*/
-/*    @Autowired
-    private Environment env;
-
     @Bean
-    public LocalSessionFactoryBean getSessionFactory() {
-        LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
-
-        Properties props = new Properties();
-        props.put(DRIVER, env.getProperty("mysql.driver"));
-        props.put(URL, env.getProperty("mysql.url"));
-        props.put(USER, env.getProperty("mysql.user"));
-        props.put(PASS, env.getProperty("mysql.password"));
-        props.put(SHOW_SQL, env.getProperty("hibernate.show_sql"));
-        props.put(HBM2DDL_AUTO, env.getProperty("hibernate.hbm2ddl.auto"));
-        props.put(DIALECT, env.getProperty("hibernate.dialect"));
-
-        factoryBean.setHibernateProperties(props);
-        factoryBean.setAnnotatedClasses(Book.class);
-        return factoryBean;
+    public HibernateTransactionManager hibernateTransactionManager() {
+        return new HibernateTransactionManager(sessionFactory());
     }
-
-    @Bean
-    public HibernateTransactionManager getTransactionManager() {
-        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
-        transactionManager.setSessionFactory(getSessionFactory().getObject());
-        return transactionManager;
-    }*/
 
 }
